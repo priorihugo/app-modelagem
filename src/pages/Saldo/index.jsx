3d756eb919bdeb5faf87de441367f00e4135f071
lchenri <lucasbonicardoso@outlook.com>
2023-06-29 15:16:26 -0300

    Atualizacao css tela

--- src/pages/Saldo/index.jsx
+++ src/pages/Saldo/index.jsx
@@ -5,6 +5,7 @@ import {
   Divider,
   FlatList,
   Heading,
+  ScrollView,
   Text,
   VStack,
 } from "native-base";
@@ -42,24 +43,33 @@ const data = [
     date: "11/11/11 - 11:11h",
     amount: -100,
   },
+  {
+    date: "11/11/11 - 11:11h",
+    amount: -100,
+  },  
+  {
+    date: "11/11/11 - 11:11h",
+    amount: -100,
+  },
 ];
 
 export default function Saldo() {
-  const saldo = "100";
-
+  const saldo = "21,90";
   return (
     <Center flex={"1"} flexDirection={"column"} backgroundColor={"#F2F2F2"}>
       <Divider />
-      <Heading>Seu saldo</Heading>
-      <Center w={"80%"} borderWidth={1} borderRadius={4} borderColor={"black"}>
-        <Text>R${saldo}</Text>
+      <Heading marginTop={10}>Seu saldo</Heading>
+      <Center w={"90%"} borderWidth={1} borderRadius={4} borderColor={"#CFDEE7"} py={2} backgroundColor={"#CFDEE7"} marginTop={3}>
+        <Text color={"#1C1D21"} fontWeight={"bold"} fontSize={16}>R${saldo}</Text>
       </Center>
       <Divider my={4}/>
 
-      <Text >Seu Extrato</Text>
+      <Heading >Seu Extrato</Heading>
 
       <FlatList
+        nestedScrollEnabled
         w={"90%"}
+        marginBottom={10}
         data={data}
         ItemSeparatorComponent={() => {
           return <Divider my={1} />;
@@ -76,15 +86,17 @@ export default function Saldo() {
               )}
 
               <Center
-                flex={"1"}
+                w={"100%"}
                 borderWidth={1}
+                borderColor={"#CFDEE7"}
                 borderRadius={4}
-                borderColor={"black"}
+                py={2}
+                backgroundColor={"#CFDEE7"}
               >
                 {item.amount < 0 ? (
-                  <Text color={"red.400"}>-R${item.amount * -1}</Text>
+                  <Text color={"red.400"} fontWeight={"bold"} fontSize={16}>-R${item.amount * -1}</Text>
                 ) : (
-                  <Text color={"green.400"}>R${item.amount}</Text>
+                  <Text color={"green.400"} fontWeight={"bold"} fontSize={16}>R${item.amount}</Text>
                 )}
               </Center>
             </Column>
